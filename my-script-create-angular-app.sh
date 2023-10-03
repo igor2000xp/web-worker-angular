d5ef2a853855954c937473d7ac1c9783a45f2785
igor2000xp <igorring@gmail.com>
2023-10-03 20:05:30 +0200

    initial commit

---
+++ my-script-create-angular-app.sh
@@ -0,0 +1,16 @@
+#bin/bash
+
+ng version
+ng new youtube-client --directory ./
+
+npm install @angular/material @angular/cdk
+
+
+ng add @angular-eslint/schematics
+
+npm install eslint-plugin-jasmine --save-dev
+
+npm install eslint-config-airbnb-typescript eslint-plugin-import --save-dev
+npm i prettier eslint-config-prettier eslint-plugin-prettier --save-dev
+
+npx eslint --init
